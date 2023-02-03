d4ea5a2eec989fdcff8b3a6eadc317668541d355
khalaimaleksandr <Khalaimaleksandr@gmail.com>
2023-02-03 18:39:26 +0300

    added greeting text, input.value = name, save name in localStorage

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ function getDate() {
 function showTime() {
   const time = document.querySelector('.time');
   time.textContent = getDate().toLocaleTimeString();
+  const greeting = document.querySelector('.greeting');
+  greeting.textContent = `Good ${getTimeOfDay(getDate().getHours())}`;
   showDate();
   setTimeout(showTime, 1000);
 }
@@ -21,4 +23,33 @@ function showDate() {
   date.textContent = getDate().toLocaleDateString('ru-RU', options);
 }
 
+
+function getTimeOfDay(hour) {
+  switch (true) {
+    case  hour >= 6 && hour < 12 :
+      return 'morning';
+    case hour >= 12 && hour < 18:
+      return 'afternoon';
+    case  hour >= 18 && hour < 24 :
+      return 'evening';
+    case  hour >= 0 && hour < 6 :
+      return 'night';
+  }
+}
+
+
+const name = document.querySelector('.name');
+
+function setLocalStorage() {
+  localStorage.setItem('name', name.value);
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+  if (localStorage.getItem('name')) {
+    name.value = localStorage.getItem('name')
+  }
+}
+
+window.addEventListener('load', getLocalStorage)
 
