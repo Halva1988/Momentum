2d3d2e7417e2f2ccb5e61dddf5a9c1e5d8a8deb4
khalaimaleksandr <Khalaimaleksandr@gmail.com>
2023-04-24 18:23:51 +0300

    changed getLinToImg to async/await

--- js/background.js
+++ js/background.js
@@ -1,41 +1,43 @@
-import { getDate } from "./getDate.js";
-import { getTimeOfDay } from "./showGreeting.js";
+import {getDate} from "./getDate.js";
+import {getTimeOfDay} from "./showGreeting.js";
 
 const limit = document.querySelector('.limitRequests');
 
 const body = document.querySelector("body");
 let linkPrevImg = '';
 let linkImg = '';
-function getLinkToImg() {
+
+async function getLinkToImg() {
   const timeOfDay = getTimeOfDay(getDate().getHours());
   if (linkImg) {
     linkPrevImg = linkImg;
   }
-  fetch(
-    `https://api.unsplash.com/photos/random?query=${ timeOfDay }&client_id=ZG90PLEE98LxvENEMAkiS9KV0l_1WXinktMxBZ2n-vQ`)
-    .then(response => response.json())
-    .then(result => {
-      limit.style.display = 'none';
-      body.style.backgroundImage = `url(${ result.urls.regular })`;
-      linkImg = result.urls.regular;
-    })
-    .catch(() => {
-      limit.style.display = 'flex';
-    })
+  const response = await fetch(`https://api.unsplash.com/photos/random?query=${getTimeOfDay(getDate().getHours())}&client_id=ZG90PLEE98LxvENEMAkiS9KV0l_1WXinktMxBZ2n-vQ`);
+  const result = await response.json();
+  let img = new Image();
+  img.onload = () => {
+    limit.style.display = 'none';
+    body.style.backgroundImage = `url(${result.urls.regular})`;
+    linkImg = result.urls.regular;
+  }
+  img.onerror = () => {
+    limit.style.display = 'flex';
+  }
+  img.src = result.urls.regular;
 }
 
-function getSlideNext() {
-  getLinkToImg();
+async function getSlideNext() {
+  await getLinkToImg();
 }
 
-function getSlidePrev() {
+async function getSlidePrev() {
   if (linkPrevImg) {
-    body.style.backgroundImage = `url(${ linkPrevImg })`;
+    body.style.backgroundImage = `url(${linkPrevImg})`;
     linkPrevImg = '';
     return;
   }
-  getLinkToImg();
+  await getLinkToImg();
 
 }
 
-export { getLinkToImg, getSlideNext, getSlidePrev };
+export {getLinkToImg, getSlideNext, getSlidePrev};
