63b8388f1d41c2bdc84089f474aa419d0013f373
Halva1988 <khalaimaleksandr@gmail.com>
2024-02-29 23:44:46 +0300

    Implement dynamic background image change based on time of day

--- js/background.js
+++ js/background.js
@@ -9,24 +9,14 @@ let linkImg = '';
 
 async function getLinkToImg() {
   const timeOfDay = getTimeOfDay(getDate().getHours());
-  if (linkImg) {
-    linkPrevImg = linkImg;
-  }
-  const response = await fetch(`https://api.unsplash.com/photos/random?query=${timeOfDay})}&client_id=ZG90PLEE98LxvENEMAkiS9KV0l_1WXinktMxBZ2n-vQ`);
-  console.log(response);
+  linkPrevImg = linkImg || '';
+  const response = await fetch(`https://api.unsplash.com/photos/random?query=${timeOfDay}&client_id=ZG90PLEE98LxvENEMAkiS9KV0l_1WXinktMxBZ2n-vQ`);
   const result = await response.json();
-  let img = new Image();
-  img.onload = () => {
-    limit.style.display = 'none';
-    linkImg = result.urls.regular;
-    body.style.backgroundImage = `url(${linkImg})`;
-  }
-  img.onerror = () => {
-    limit.style.display = 'flex';
-  }
-
-  img.src = result.urls.regular;
+  linkImg = result.urls.regular;
+  body.style.backgroundImage = `url(${linkImg})`;
+  limit.style.display = 'none';
 }
+
 
 async function getSlidePrev() {
   if (linkPrevImg) {
